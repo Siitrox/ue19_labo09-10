6cbaa8d97b38d060d81a3a8415c590c43479f5a5
Siitrox <maxhmn.perso@protonmail.com>
2023-11-13 15:07:57 +0100

    Amélioration de l'application et ajout du fichier Dockerfile et requirements.txt

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3
+
+WORKDIR /home/archusr/School/B2/Q1/UE19-Principes_avancés/Exercice/Labo_5/ue19_labo09-10
+
+copy requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD [ "python", "./app.py" ]
--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 # Programme interrogeant l'API
+import requests
+
+
 def main():
-    return None
+    url = 'https://icanhazdadjoke.com/'
+
+    response = requests.get(url, headers={"Accept": "application/json"})
+
+    print("Here is your dad's best joke : " + response.json()['joke'])
 
 
 if __name__ == '__main__':
